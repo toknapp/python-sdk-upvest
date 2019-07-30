7c386ed1f4de8c53817588e0df06bf512c966970
Carl Crowder <carl@redcliff.ltd>
2019-07-30 12:43:24 +0200

    Tidying up packaging format and simlifying CI

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 import os
 import setuptools
 
+_VERSION = "0.0.6"
+
 _README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
-_REQUIREMENTS = [
-    line.strip() for line in open(os.path.join(os.path.dirname(__file__),'requirements.upvest.txt')).readlines()
-]
+_INSTALL_REQUIRES = ('requests>=2.21.0,<3', 'environs==4.1.0')
+_OPTIONAL_REQUIRES = {
+    'dev': ("pre-commit==1.10.5", "prospector==1.1.6.2"),
+    'test': ('pytest==4.2.0', 'py-ecc==1.7.0', 'pysha3==1.0.2', 'bitcoin==1.1.42')
+}
 
 _CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 3.7',
+    "Programming Language :: Python :: 3.8",
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
@@ -28,19 +34,20 @@ _PACKAGES = setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
 
 setuptools.setup(
     name="upvest",
-    version="0.0.6",
+    version=_VERSION,
     author="Upvest GmbH",
     author_email="tech@upvest.co",
     maintainer="Alexander Reichhardt",
     maintainer_email="alexander@upvest.co",
     description="Upvest API client library",
     keywords=_KEYWORDS,
-    license='MIT',
     long_description=_README,
     long_description_content_type="text/markdown",
     url="https://github.com/toknapp/python-sdk-upvest/",
     packages=_PACKAGES,
     include_package_data=True,
     classifiers=_CLASSIFIERS,
-    install_requires=_REQUIREMENTS,
+    install_requires=_INSTALL_REQUIRES,
+    extras_require=_OPTIONAL_REQUIRES,
+    license='MIT',
 )
